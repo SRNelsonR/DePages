d80971d3cbb0784f28a24600f278c147040ffb22
SRNelsonR <oscarnelson18@gmail.com>
2018-12-17 13:10:55 -0600

    Registrarse
    Módulo de registrarse

--- DePages/index.js
+++ DePages/index.js
@@ -1,5 +1,92 @@
 var express = require("express");
+var session = require("express-session");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+
 var app = express();
+
+var credenciales = {
+    host:"localhost",
+    user:"root",
+    password:"",
+    port:3306,
+    database:"db_depages"
+};
+
 app.use(express.static("public"));
-app.listen(8001);
-console.log("Servidor levantado con éxito.");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({secret:"ASDFE$%#%",resave:true, saveUninitialized:true}));
+
+//Verificar si existe una variable de sesion para poner publica la carpeta public-depages
+var publicDepages = express.static("public-depages");
+app.use(
+    function(req,res,next){
+        if (req.session.correoUsuario){
+            //Significa que el usuario si esta logueado
+            publicDepages(req,res,next);
+        }
+        else
+            return next();
+    }
+);
+
+/*----------------------Peticiones para Registrarse-----------------------------------------------------*/
+
+//Petición para cargar los generos
+app.get("/cargar-genero", function(req, res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        "SELECT codigo_genero, nombre_genero FROM tbl_generos;",
+        [],
+        function(error, data, fields){
+            res.send(data);
+            res.end();
+        }    
+    ); 
+});
+
+//Peticióno para cargar los planes
+app.get("/cargar-plan", function(req, res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        "select codigo_plan, nombre_plan, cantidad from tbl_planes",
+        [],
+        function(error, data, fields){
+            res.send(data);
+            res.end();
+        }    
+    ); 
+});
+
+// Petición para crear un usuario
+app.post("/crear-usuario", function(req, res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(
+        "INSERT INTO tbl_usuarios (codigo_genero, codigo_plan, nombre, apellido, correo, nickname, contrasena, fecha_creacion) VALUES (?, ?, ?, ?, ?, ?, ?, '12/11/18')",
+        [
+            req.body.genero,
+            req.body.plan,
+            req.body.nombre,
+            req.body.apellido,
+            req.body.correo,
+            req.body.nickname,
+            req.body.contrasena
+        ],
+        function(error, data, fields){
+            if (error){
+                res.send(error);
+                res.end();
+            }else{
+                res.send(data);
+                res.end();
+            }
+        }
+    );
+    conexion.end();
+});
+
+
+app.listen(8001, function(){
+    console.log("Servidor levantado con éxito.");
+});
---
+++ DePages/public/js/controlador-registrarse.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    //Cargar género
+    $.ajax({
+        url:"/cargar-genero",
+        method:"GET",
+        dataType:"json",
+        success:function(res){
+            res.forEach(function(element) {
+                $("#slc-genero").append(
+                    `<option value="${element.codigo_genero}">${element.nombre_genero}</option>`
+                );
+            });
+        },
+        error:function(error){
+            console.error(error);
+        }
+    });
+
+    //Cargar Tipo Plan
+    $.ajax({
+        url:"/cargar-plan",
+        method:"GET",
+        dataType:"json",
+        success:function(res){
+            res.forEach(function(element) {
+                $("#slc-plan").append(
+                    `<option value="${element.codigo_plan}">${element.nombre_plan}</option>`
+                );
+            });
+        },
+        error:function(error){
+            console.error(error);
+        }
+    });
+});
+
+
+//Función para crear nuevo usuario verificando que los campos esten llenos
+$("#btn-crear").click(function(){
+    var datos = "nombre=" + $("#txt-nombre").val() + "&apellido=" + $("#txt-apellido").val() + "&correo=" +$("#txt-correo").val() + "&contrasena=" + $("#txt-password").val() + "&nickname=" + $("#txt-usuario").val() + "&genero=" + $("#slc-genero").val() + "&plan=" + $("#slc-plan").val();
+    if( 
+        $("#txt-nombre").val() == "" ||
+        $("#txt-apellido").val() == "" ||
+        $("#txt-correo").val() == "" ||
+        $("#txt-password").val() == "" ||
+        $("#txt-usuario").val() == "" ||
+        $("#txt-genero").val() == "" ||
+        $("#txt-plan").val() == ""
+    ){
+        //alert("valores vacios");
+        $("#error").fadeIn().delay(3000).fadeOut();
+    }else{
+        //alert("Llenos");        
+        $.ajax({
+            url:"/crear-usuario",
+            method:"POST",
+            data:datos,
+            dataType:"json",
+            success:function(res){
+                console.log(res);
+                $("#creado").fadeIn().delay(3000).fadeOut();
+                window.setTimeout(function(){
+                    window.location.href = "/login.html";
+                }, 5000);
+                $("#btn-crear").prop('disabled', true)
+            },
+            error:function(error){
+                console.error(error);
+            }
+        });
+    }
+});
